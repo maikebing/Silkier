2e3cd4c87621af73c6653acee082371c472f90fe
MysticBoy <mysticboy@live.com>
2020-02-26 03:52:46 +0800

    加入并行计算中使用依赖注入

---
+++ Demos/ParalleDI/Program.cs
@@ -0,0 +1,173 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Silkier.Extensions;
+
+namespace Demos
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+    public class BlogX
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+
+    public class BloggingContext : DbContext
+    {
+        public static long InstanceCount;
+
+        public BloggingContext(DbContextOptions options)
+            : base(options)
+            => Interlocked.Increment(ref InstanceCount);
+
+        public DbSet<Blog> Blogs { get; set; }
+    }
+
+    public class BlogController
+    {
+        private readonly BloggingContext _context;
+
+        public BlogController(BloggingContext context) => _context = context;
+
+        public async Task ActionAsync() => await _context.Blogs.FirstAsync();
+    }
+
+    public class Startup
+    {
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextPool<BloggingContext>(c => c.UseInMemoryDatabase("test"));
+            services.AddSingleton(b =>
+            {
+                return new Blog() { BlogId = 1, Name = "Blog", Url = "http://www.cn.cn" };
+            });
+            services.AddTransient(b =>
+            {
+                return new BlogX() { BlogId = 1, Name = "BlogX", Url = "http://www.cn.cn" };
+            });
+        }
+    }
+
+    public class Program
+    {
+        private const int Threads = 32;
+        private const int Seconds = 10;
+
+        private static long _requestsProcessed;
+
+        private static async Task Main()
+        {
+            var serviceCollection = new ServiceCollection();
+            new Startup().ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            SetupDatabase(serviceProvider);
+
+            var stopwatch = new Stopwatch();
+
+            MonitorResults(TimeSpan.FromSeconds(Seconds), stopwatch);
+
+            await Task.WhenAll(
+                Enumerable
+                    .Range(0, Threads).ToArray()
+                    .Select(async _ => await SimulateRequestsAsync(serviceProvider, stopwatch)).ToArray());
+        }
+
+        private static void SetupDatabase(IServiceProvider serviceProvider)
+        {
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<BloggingContext>();
+
+                if (context.Database.EnsureCreated())
+                {
+                    context.Blogs.Add(new Blog { Name = "The Dog Blog", Url = "http://sample.com/dogs" });
+                    context.Blogs.Add(new Blog { Name = "The Cat Blog", Url = "http://sample.com/cats" });
+                    context.SaveChanges();
+                }
+            }
+            int x = 0;
+            List<int> list = Enumerable.Range(1, 1000).ToList();
+            ParallelPart.ForEach(list, 2, serviceProvider, (int i, Blog b, BlogX x, BloggingContext bc) =>
+           {
+               if (!bc.Blogs.Any(b => b.BlogId == i))
+               {
+                   bc.Blogs.Add(new Blog() {  Name= b.Name, Url= b.Url});
+                   Console.WriteLine($"{Thread.CurrentThread} {i}{b?.Name}{x?.Name}");
+                   bc.SaveChanges();
+               }
+           });
+        }
+
+        private static async Task SimulateRequestsAsync(IServiceProvider serviceProvider, Stopwatch stopwatch)
+        {
+            while (stopwatch.IsRunning)
+            {
+                try
+                {
+                    using (var serviceScope = serviceProvider.CreateScope())
+                    {
+                        await new BlogController(serviceScope.ServiceProvider.GetService<BloggingContext>()).ActionAsync();
+                    }
+                    Interlocked.Increment(ref _requestsProcessed);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+          
+            }
+           await  Task.CompletedTask;
+        }
+
+        private static async void MonitorResults(TimeSpan duration, Stopwatch stopwatch)
+        {
+            var lastInstanceCount = 0L;
+            var lastRequestCount = 0L;
+            var lastElapsed = TimeSpan.Zero;
+
+            stopwatch.Start();
+
+            while (stopwatch.Elapsed < duration)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+                var instanceCount = BloggingContext.InstanceCount;
+                var requestCount = _requestsProcessed;
+                var elapsed = stopwatch.Elapsed;
+                var currentElapsed = elapsed - lastElapsed;
+                var currentRequests = requestCount - lastRequestCount;
+
+                Console.WriteLine(
+                    $"[{DateTime.Now:HH:mm:ss.fff}] "
+                    + $"Context creations/second: {instanceCount - lastInstanceCount} | "
+                    + $"Requests/second: {Math.Round(currentRequests / currentElapsed.TotalSeconds)}");
+
+                lastInstanceCount = instanceCount;
+                lastRequestCount = requestCount;
+                lastElapsed = elapsed;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Total context creations: {BloggingContext.InstanceCount}");
+            Console.WriteLine(
+                $"Requests per second:     {Math.Round(_requestsProcessed / stopwatch.Elapsed.TotalSeconds)}");
+
+            stopwatch.Stop();
+        }
+    }
+}
--- Silkier/ParallelPart.cs
+++ Silkier/ParallelPart.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,6 +35,50 @@ namespace Silkier.Extensions
             });
         }
 
+
+   
+        public static ParallelLoopResult ForEach<T, T1>(IEnumerable<T> source, ParallelOptions  options, IServiceProvider factory, Action<T, T1> action)
+                                    => ForEach<T, Action<T, T1>>(source, options, factory, action);
+
+         
+        public static ParallelLoopResult ForEach<T, T1, T2>(IEnumerable<T> source, int _maxDegreeOfParallelism, IServiceProvider factory, Action<T, T1, T2> action)
+                                    => ForEach(source, new ParallelOptions() { MaxDegreeOfParallelism = _maxDegreeOfParallelism }, factory, action);
+
+        public static ParallelLoopResult ForEach<T, T1, T2,T3>(IEnumerable<T> source,int  _maxDegreeOfParallelism  , IServiceProvider factory, Action<T, T1, T2,T3> action)
+                                    => ForEach(source,  new ParallelOptions() { MaxDegreeOfParallelism= _maxDegreeOfParallelism }, factory, action);
+
+        public static ParallelLoopResult ForEach<T, T1, T2,T3, T4>(IEnumerable<T> source, int _maxDegreeOfParallelism, IServiceProvider factory, Action<T, T1, T2, T3,T4> action)
+                               => ForEach(source, new ParallelOptions() { MaxDegreeOfParallelism = _maxDegreeOfParallelism }, factory, action);
+
+        public static ParallelLoopResult ForEach<T, T1, T2, T3,T4,T5>(IEnumerable<T> source, int _maxDegreeOfParallelism, IServiceProvider factory, Action<T, T1, T2, T3,T4,T5> action)
+                               => ForEach(source, new ParallelOptions() { MaxDegreeOfParallelism = _maxDegreeOfParallelism }, factory, action);
+
+      
+
+
+
+        private static ParallelLoopResult ForEach<T, A>(IEnumerable<T> source,  ParallelOptions parallelOptions, IServiceProvider factory, A action) where A : Delegate
+        {
+            int rangeSize = (source.Count() + parallelOptions.MaxDegreeOfParallelism - 1) / parallelOptions.MaxDegreeOfParallelism;
+            return Parallel.ForEach(Partitioner.Create(0, source.Count(), Math.Min(source.Count(), rangeSize)), parallelOptions ?? new ParallelOptions(), (range, loopState) =>
+            {
+                using (var scope = factory.CreateScope())
+                {
+                    List<object> list = new List<object>();
+                    action.GetType().GenericTypeArguments.Skip(1).ToList().ForEach(t =>
+                    {
+                        list.Add(scope.ServiceProvider.GetService(t));
+                    });
+                    for (int i = range.Item1; i < range.Item2; i++)
+                    {
+                        var objs = new List<object>();
+                        objs.Add(source.ElementAt(i));
+                        objs.AddRange(list);
+                        action.DynamicInvoke(objs.ToArray());
+                    }
+                }
+            });
+        }
 
         /// <summary>
         /// 并行处理<paramref name="source"/>，按照最多<paramref name="_maxDegreeOfParallelism"/>个个数分配元素给<paramref name="action"/>处理
